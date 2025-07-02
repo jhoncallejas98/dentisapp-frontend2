6d9d82c8f0f10f25cf11f51c55b3540b7b0ae538
Jhon <jhoncallejas1998@hotmail.com>
2025-07-01 21:05:31 -0500

    login hasta ahora con modificaciones ne css no funcional aun

--- src/app/pages/public/login/login.ts
+++ src/app/pages/public/login/login.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  formData!: FormGroup;
 
+  constructor() {
+    this.formData = new FormGroup({
+      userName: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('',[Validators.required, Validators.minLength(6), Validators.maxLength(12)] )
+    })
+  }
+
+  onSubmit(){
+    if (this.formData.valid){
+      console.log(this.formData.value);
+    }
+  }
 }
